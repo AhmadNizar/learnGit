75bfcfc476e990458310f70a985934d0b9694a90
Ahmad Nizar <ahmadnizar.owl@gmail.com>
2019-05-15 08:28:33 +0700

    second commit

--- learnGit/ViewController.swift
+++ learnGit/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("test first commit")
+        print("test second commit")
     }
 
 
